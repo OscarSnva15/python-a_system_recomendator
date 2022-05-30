90f20aaf9808cdb80550195d5745a0019f0f8fb5
oscarsnva15@gmail.com <oscarsnva15@gmail.com>
2022-05-30 16:28:03 -0500

    add: convertion grate to km

--- code/data.py
+++ code/data.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-# Python 3 program to calculate Distance Between Two Points on Earth
 from math import radians, cos, sin, asin, sqrt
 from requests import head
-#origin_cordenate
-cx= 53.32055555555556
-cy=-1.7297222222222221
+#origin point microbusiness
+latitude1= 19.620550757350234
+longitude1=-99.31328114851462
 
 def distance(lat1, lat2, lon1, lon2):
     # The math module contains a function named
@@ -20,19 +19,17 @@ def distance(lat1, lat2, lon1, lon2):
     dlat = lat2 - lat1
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * asin(sqrt(a))
-    
     # Radius of earth in kilometers. Use 3956 for miles
     r = 6371
-      
     # calculate the result
     return(c * r)
 
-# load cvs
-df=pd.read_csv('../DENUE/test.csv')
+#load cvs
+df=pd.read_csv('../consultas/nicolasRomero.csv')
+print(df.head())
 
-# print(pd.unique(df['Latitud']))
-# print(pd.unique(df['Longitud']))
-print('------')
-for longitud in df['Longitud']:
-    for latitud in df['Latitud']:
-        print(distance(latitud, cy, longitud, cx), "K.M")
+print('---DISTANCES---')
+for i in df.index:
+    print(distance(latitude1, df['Latitud'][i], longitude1, df['Longitud'][i]), "K.M")
+#coordenadas little cesar el charco
+#19.621621977935146, -99.31497630458834
